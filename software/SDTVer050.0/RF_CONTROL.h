db676e64c0e4b39ea26368063c2f6ab63a6254ab
Justin Giorgi <justin@ai6ym.radio>
2024-11-16 17:46:59 -0800

    Removing previous main software release. Keeping software in version-less directory.

--- software/SDTVer050.0/RF_CONTROL.h
+++
@@ -1,9 +0,0 @@
-#ifndef RF_CONTROL_h
-#define RF_CONTROL_h
-
-void RFControlInit();
-void RFControl_Enable_Prescaler(bool status);
-void SetRF_InAtten(int attenIn);
-void SetRF_OutAtten(int attenOut);
-
-#endif // RF_CONTROL_H
